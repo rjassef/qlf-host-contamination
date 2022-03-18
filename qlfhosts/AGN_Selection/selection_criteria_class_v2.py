019263afb647e4c22b26754368fe4692aadc129e
rjassef <roberto.assef@mail.udp.cl>
2022-03-18 15:26:53 -0300

    Minor changes.

--- qlfhosts/AGN_Selection/selection_criteria_class_v2.py
+++ qlfhosts/AGN_Selection/selection_criteria_class_v2.py
@@ -12,35 +12,9 @@ class SelectionCriteria(object):
     def is_selected(self, Lhost_Lagn, Lhost_Lagn_unscaled, agn_mags, mag_diff):
 
         #Set the normalization for the SED ratios.
-        #ratio = Lhost_Lagn/Lhost_Lagn_unscaled[k_hosts_sed]
         ratio = Lhost_Lagn/Lhost_Lagn_unscaled
 
-        #Figure out the reddening factors for the AGN. 
-        # Aband = np.zeros(len(agn_sed.bps))
-        # klam_norm = agn_sed.klam(lam_rest)
-        # for j, bp in enumerate(agn_sed.bps):
-        #     lam_eff_band = bp.avgwave()
-        #     Aband[j] = (agn_sed.klam(lam_eff_band/(1+agn_sed.z)) / klam_norm) * Alam
-
-        #Get the AGN magnitudes.
-        # agn_mags = dict()
-        # for j, bp_name in enumerate(agn_sed.bp_names):
-        #     agn_mags[bp_name] = agn_sed.mag_unscaled[0][j] + Aband[j]
-
-        #Get the host magnitudes. 
-        # host_mags = dict()
-        # for j, bp_name in enumerate(hosts_sed.bp_names):
-        #     host_mags[bp_name] = hosts_sed.mag_unscaled[k_hosts_sed][j]
-
-        #Combine the magnitudes.
-        # try:
-        #     mag = dict()
-        #     for bp_name in agn_mags:
-        #         mag[bp_name] = -2.5*np.log10(10**(-0.4*agn_mags[bp_name]) + ratio * 10**(-0.4*host_mags[bp_name]))
-        # except KeyError:
-        #     print("Need to use the same bands in AGN and hosts SEDs.")
-        #     sys.exit()
-   
+        #Get the magnitudes.  
         mag = dict()
         for bp_name in agn_mags:
             mag[bp_name] = agn_mags[bp_name] - 2.5*np.log10(1.+ratio*10**(-0.4*mag_diff[bp_name]))
--- qlfhosts/util/Lhost_Lagn_max_v2.py
+++ qlfhosts/util/Lhost_Lagn_max_v2.py
@@ -4,17 +4,6 @@ import sys
 #Function to find the maximum tolerated ratio between the host and AGN at wavelength lambda for which the object would be selected as an AGN according to criterion sel_crit.
 def Lhost_Lagn_max(agn_sed, hosts_sed, Alam, Aband_to_Alam, sel_crit, x_unscaled_all, xlow=0, xhig=100, niter_max=100, debug=False):
 
-    #Find the unscaled luminosity ratio. 
-    #Lnu_agn_unscaled = agn_sed.Lnu(lam_rest)
-    #Lnu_hosts_unscaled = hosts_sed.Lnu(lam_rest)
-
-    #Figure out the reddening factors for the AGN. 
-    # Aband = np.zeros(len(agn_sed.bps))
-    # klam_norm = agn_sed.klam(lam_rest)
-    # for j, bp in enumerate(agn_sed.bps):
-    #     lam_eff_band = bp.avgwave()
-    #     Aband[j] = (agn_sed.klam(lam_eff_band/(1+agn_sed.z)) / klam_norm) * Alam
-
     #Get the AGN unscaled magnitudes.
     agn_mags = dict()
     for j, bp_name in enumerate(agn_sed.bp_names):
@@ -35,26 +24,21 @@ def Lhost_Lagn_max(agn_sed, hosts_sed, Alam, Aband_to_Alam, sel_crit, x_unscaled
     x = np.zeros(len(hosts_sed.sps))
     for k, x_unscaled in enumerate(x_unscaled_all):
 
-        # #Set the normalization factor for this template.
-        # x_unscaled = Lnu_host_unscaled/Lnu_agn_unscaled[0]
-
-        #Isolate only the mag_diff values needed.
-        mag_diff = all_hosts_mag_diff[k]
-
         #Don't go through this if the host sed template has 0 likelihood (i.e., is not being used)
         if hosts_sed.likelihood[k]==0.:
             x[k] = 0.
             continue
 
-        #Check that xmin selects the source and that xmax does not. If this is not met, then return the bound.
-        #if not sel_crit.is_selected(xlow, Alam, lam_rest, agn_sed, hosts_sed, k, x_unscaled):
+        #Isolate only the mag_diff values needed.
+        mag_diff = all_hosts_mag_diff[k]
+
+        #Check that xmin selects the source and that xmax does not. If this is not met, 
         if not sel_crit.is_selected(xlow, x_unscaled, agn_mags, mag_diff):
             if debug:
                 print("Object not selected as AGN at xlow. Returning xlow.")
             x[k] = xlow
             continue
 
-        #if sel_crit.is_selected(xhig, Alam, lam_rest, agn_sed, hosts_sed, k, x_unscaled):
         if sel_crit.is_selected(xhig, x_unscaled, agn_mags, mag_diff):
             if debug:
                 print("Object selected as AGN at xhig. Returning xhig.")
@@ -66,7 +50,6 @@ def Lhost_Lagn_max(agn_sed, hosts_sed, Alam, Aband_to_Alam, sel_crit, x_unscaled
         x2 = xhig
         for i in range(niter_max):
             x[k] = 0.5*(x1+x2)
-            #if sel_crit.is_selected(x[k], Alam, lam_rest, agn_sed, hosts_sed, k, x_unscaled):
             if sel_crit.is_selected(x[k], x_unscaled, agn_mags, mag_diff):
                 x1 = x[k]
             else:
@@ -79,7 +62,6 @@ def Lhost_Lagn_max(agn_sed, hosts_sed, Alam, Aband_to_Alam, sel_crit, x_unscaled
             print("Exceed niter_max={} iterations. Returning current solution.".format(niter_max))
         continue
 
-    #print(x)
     return x
 
         
